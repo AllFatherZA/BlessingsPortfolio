486c6847aa948a09ec9084be2691e3dda971243b
allfatherza <mnyandenilunga@gmail.com>
2023-06-06 10:38:01 +0000

    Updates

---
+++ precache-manifest.a31ae07f0242506ea158c24aa6e7539b.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "c20e5745108391ec1550",
+    "url": "/TwiskPortfolio/static/css/main.d0d56382.chunk.css"
+  },
+  {
+    "revision": "c20e5745108391ec1550",
+    "url": "/TwiskPortfolio/static/js/main.c20e5745.chunk.js"
+  },
+  {
+    "revision": "e528be91ed1c5fff8769",
+    "url": "/TwiskPortfolio/static/css/1.db66f4c6.chunk.css"
+  },
+  {
+    "revision": "e528be91ed1c5fff8769",
+    "url": "/TwiskPortfolio/static/js/1.e528be91.chunk.js"
+  },
+  {
+    "revision": "a4731731d012972194f8",
+    "url": "/TwiskPortfolio/static/js/runtime~main.a4731731.js"
+  },
+  {
+    "revision": "9ebb86aafeecaa052ce1ea36d366cfb5",
+    "url": "/TwiskPortfolio/static/media/twisklogo.9ebb86aa.png"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/TwiskPortfolio/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/TwiskPortfolio/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/TwiskPortfolio/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/TwiskPortfolio/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/TwiskPortfolio/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/TwiskPortfolio/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/TwiskPortfolio/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/TwiskPortfolio/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "18e614b6dd9a29641ad61ace914c2e11",
+    "url": "/TwiskPortfolio/static/media/FourierA.18e614b6.JPG"
+  },
+  {
+    "revision": "b03c258e62369ca95260f801e1b7902e",
+    "url": "/TwiskPortfolio/static/media/BuyLowSellHigh.b03c258e.JPG"
+  },
+  {
+    "revision": "8c64c75eba9b0349e4488b3fe121234f",
+    "url": "/TwiskPortfolio/static/media/VarModels.8c64c75e.JPG"
+  },
+  {
+    "revision": "4247505e80e2f5b011e0388329f00363",
+    "url": "/TwiskPortfolio/static/media/QuantTrading.4247505e.JPG"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/TwiskPortfolio/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/TwiskPortfolio/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TwiskPortfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/TwiskPortfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/TwiskPortfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/TwiskPortfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/TwiskPortfolio/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "6a2a16c6e78e800ad98d3858bc16aea5",
+    "url": "/TwiskPortfolio/static/media/bg-image1.6a2a16c6.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/TwiskPortfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/TwiskPortfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/TwiskPortfolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "7f477633ddd12f84284654f2a2e89b8a",
+    "url": "/TwiskPortfolio/static/media/bootstrap-icons.7f477633.woff2"
+  },
+  {
+    "revision": "52196284de1fcb5b044f001a75482dba",
+    "url": "/TwiskPortfolio/static/media/bootstrap-icons.52196284.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/TwiskPortfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/TwiskPortfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/TwiskPortfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/TwiskPortfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/TwiskPortfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "60629cef58215144fe1f0507dc2c503c",
+    "url": "/TwiskPortfolio/index.html"
+  }
+];
